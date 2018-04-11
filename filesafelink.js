42b63918c047ee4fe458b66442ed3814c33befe0
zhafran21 <37872765+zhafran21@users.noreply.github.com>
2018-04-11 20:18:40 +0700

    Update filesafelink.js

--- filesafelink.js
+++ filesafelink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://pakpol.xyz/2018/04/11/hello-world/?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://pakpol.xyz/process/?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
